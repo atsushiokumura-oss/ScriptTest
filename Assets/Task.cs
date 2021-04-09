ada7afb80f8324de1003040922bc1d03620101ac
atsushiokumura-oss <81137612+atsushiokumura-oss@users.noreply.github.com>
2021-04-09 16:38:27 +0900

    Initial commit

---
+++ Assets/Task.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] points = {10, 40, 50, 70, 100};
+        for (int i = 0; i < points.Length; i++)
+        {
+            Debug.Log(points[i]);
+        }
+
+        for (int i = 1; i <= points.Length; i++)
+        {
+            int j = points.Length-i;
+            Debug.Log(points[j]);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージ受けた");
+        this.hp -= damage;
+    }
+
+    public void Magic()
+    {
+
+        
+        if (this.mp >= 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+
+        }
+        else
+        { 
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+}
+
+
+
+public class Test : MonoBehaviour
+{
+    
+    
+
+    void Start ()
+    {
+        Boss lastboss = new Boss();
+        lastboss.Attack();
+        lastboss.Defence(3);
+        lastboss.Magic();
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
